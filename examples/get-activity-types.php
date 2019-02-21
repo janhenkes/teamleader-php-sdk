143570c4e04c343270fb1c9ae6a30d77bf9102e7
Verstraete Kevin <kevin@sumocoders.be>
2019-02-21 16:24:18 +0100

    Example to get all the activity types

---
+++ examples/get-activity-types.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__ . '/credentials.php';
+
+$connection = new \Teamleader\Connection();
+$connection->setClientId( $clientId );
+$connection->setClientSecret( $clientSecret );
+
+$client = new \Teamleader\Client( $connection );
+
+$activityTypes = $client->activityType()->get();
+
+var_dump( $activityTypes );
