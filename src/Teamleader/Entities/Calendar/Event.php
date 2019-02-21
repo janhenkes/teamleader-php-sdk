c6febcc9c0106e820dca0e95ab8a0ec8104aebe1
Verstraete Kevin <kevin@sumocoders.be>
2019-02-21 18:05:08 +0100

    Event entity

---
+++ src/Teamleader/Entities/Calendar/Event.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Teamleader\Entities\Calendar;
+
+use Teamleader\Actions\FindAll;
+use Teamleader\Actions\Storable;
+use Teamleader\Model;
+
+class Event extends Model
+{
+    use Storable;
+    use FindAll;
+
+    // define the action because some endpoints use add and some use create
+    private $createAction = 'create';
+
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'activity_type_id',
+        'starts_at',
+        'ends_at',
+        'location',
+        'work_type_id',
+        'attendees', // { "type":"contact", "id":"" }
+        'links', // { "type":"contact", "id":"" }
+    ];
+
+    /**
+     * @var string
+     */
+    protected $endpoint = 'events';
+}
