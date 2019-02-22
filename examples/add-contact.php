1e114b0d256de862e9beb8d50db3ec8b246556a4
Verstraete Kevin <kevin@sumocoders.be>
2019-02-22 13:45:21 +0100

    Example to add a contact

---
+++ examples/add-contact.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__ . '/credentials.php';
+
+$connection = new \Teamleader\Connection();
+$connection->setClientId( $clientId );
+$connection->setClientSecret( $clientSecret );
+
+$client = new \Teamleader\Client( $connection );
+
+$contact = $client->contact(
+    [
+        'first_name' => 'Chuck',
+        'last_name'  => 'Norris',
+    ]
+);
+
+$contact->save();
+
+var_dump( $contact );
