bb8b40404dadcbb58e89f758f031c00d7133cf74
Verstraete Kevin <kevin@sumocoders.be>
2019-02-25 17:30:13 +0100

    Add invoice entity

---
+++ src/Teamleader/Entities/Invoicing/Invoice.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Teamleader\Entities\Invoicing;
+
+use Teamleader\Actions\FindAll;
+use Teamleader\Model;
+
+class Invoice extends Model
+{
+    use FindAll;
+
+    protected $fillable = [
+        'invoicee', // { "customer": { "type": "contact", "id" : "" }, "for_attention_of" : { "name": "" OR "contact_id" : "" } }
+        'department_id',
+        'payment_term', // { "type": "cash", "days" : "" }
+        'grouped_lines',
+        'invoice_date',
+        'discounts',
+        'note',
+        'custom_fields',
+    ];
+
+    /**
+     * @var string
+     */
+    protected $endpoint = 'invoices';
+}
