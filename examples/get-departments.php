a333b732d8a24615afecbb870d2210034c6c35c2
Verstraete Kevin <kevin@sumocoders.be>
2019-02-26 10:22:03 +0100

    Example to get a list of all departments

---
+++ examples/get-departments.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+require __DIR__ . '/credentials.php';
+
+$connection = new \Teamleader\Connection();
+$connection->setClientId( $clientId );
+$connection->setClientSecret( $clientSecret );
+
+$client = new \Teamleader\Client( $connection );
+
+$departments = $client->department()->get();
+
+var_dump( $departments );
