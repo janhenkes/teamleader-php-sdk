2d3f1552ac865db5d3a2c925c71430594d636e0c
Verstraete Kevin <kevin@sumocoders.be>
2019-02-26 10:19:59 +0100

    Set the invoice default create action as draft

--- src/Teamleader/Entities/Invoicing/Invoice.php
+++ src/Teamleader/Entities/Invoicing/Invoice.php
@@ -3,11 +3,15 @@
 namespace Teamleader\Entities\Invoicing;
 
 use Teamleader\Actions\FindAll;
+use Teamleader\Actions\Storable;
 use Teamleader\Model;
 
 class Invoice extends Model
 {
     use FindAll;
+    use Storable;
+
+    private $createAction = 'draft';
 
     protected $fillable = [
         'invoicee', // { "customer": { "type": "contact", "id" : "" }, "for_attention_of" : { "name": "" OR "contact_id" : "" } }
