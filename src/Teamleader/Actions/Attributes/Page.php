7ac0eb0e665c34d80126dcd997b71d449cac4330
Jelmer Prins <jelmer@sumocoders.be>
2019-02-28 16:04:42 +0100

    Page attribute

---
+++ src/Teamleader/Actions/Attributes/Page.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Teamleader\Actions\Attributes;
+
+final class Page
+{
+    private const DEFAULT_SIZE = 20;
+    private const DEFAULT_NUMBER = 1;
+    private $size;
+    private $number;
+
+    public function __construct(int $size = self::DEFAULT_SIZE, int $number = self::DEFAULT_NUMBER)
+    {
+        $this->size = $this->isValidNumber($size) ? $size : self::DEFAULT_SIZE;
+        $this->number = $this->isValidNumber($number) ? $number : self::DEFAULT_NUMBER;
+    }
+
+    private function isValidNumber(int $number): bool
+    {
+        return $number >= 1;
+    }
+
+    public function getSize(): int
+    {
+        return $this->size;
+    }
+
+    public function getNumber(): int
+    {
+        return $this->number;
+    }
+}
