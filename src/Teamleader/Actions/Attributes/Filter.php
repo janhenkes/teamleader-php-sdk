1cc71225f90a6e888e73d313454d0cd255ad0ff6
Jelmer Prins <jelmer@sumocoders.be>
2019-02-28 15:23:13 +0100

    Filter attribute

---
+++ src/Teamleader/Actions/Attributes/Filter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Teamleader\Actions\Attributes;
+
+class Filter
+{
+    /**
+     * These can be overwitten in filters specific for a certain entity to aid with validation
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array
+     */
+    protected $filters;
+
+    public function __construct(array $filters = [])
+    {
+        $this->fill($filters);
+    }
+
+    public function addFilter(string $key, $value): self
+    {
+        if ($this->isFillable($key)) {
+            $this->filters[$key] = $value;
+        }
+
+        return $this;
+    }
+
+    public function getFilters(): array
+    {
+        return $this->filters;
+    }
+
+    protected function fill(array $filters): void
+    {
+        foreach ($filters as $key => $value) {
+            $this->addFilter($key, $value);
+        }
+    }
+
+    protected function isFillable(string $key): bool
+    {
+        if (count($this->fillable) > 0) {
+            return in_array($key, $this->fillable);
+        }
+
+        return true;
+    }
+}
