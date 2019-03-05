68f20efdb7d2196995be830c9641d75ff7559dfd
Jelmer Prins <jelmer@sumocoders.be>
2019-03-05 11:00:27 +0100

    Implement json serialisation for the invoice entity so we can use it for a reference

--- src/Teamleader/Entities/Invoicing/Invoice.php
+++ src/Teamleader/Entities/Invoicing/Invoice.php
@@ -5,15 +5,19 @@ namespace Teamleader\Entities\Invoicing;
 use Teamleader\Actions\FindAll;
 use Teamleader\Actions\Storable;
 use Teamleader\Model;
+use JsonSerializable;
 
-class Invoice extends Model
+class Invoice extends Model implements JsonSerializable
 {
     use FindAll;
     use Storable;
 
+    const TYPE = 'invoice';
+
     private $createAction = 'draft';
 
     protected $fillable = [
+        'id',
         'invoicee', // { "customer": { "type": "contact", "id" : "" }, "for_attention_of" : { "name": "" OR "contact_id" : "" } }
         'department_id',
         'payment_term', // { "type": "cash", "days" : "" }
@@ -28,4 +32,12 @@ class Invoice extends Model
      * @var string
      */
     protected $endpoint = 'invoices';
+
+    public function jsonSerialize(): object
+    {
+        return (object) [
+            'type' => self::TYPE,
+            'id' => $this->id,
+        ];
+    }
 }
