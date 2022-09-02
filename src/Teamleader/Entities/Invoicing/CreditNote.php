049c903cd143d8d3bc662112612ea01826cb1c7e
Hervé Delvaux <herve.delvaux@webadev.com>
2022-09-02 15:59:17 +0200

    FindById + Donwload

--- src/Teamleader/Entities/Invoicing/CreditNote.php
+++ src/Teamleader/Entities/Invoicing/CreditNote.php
@@ -3,11 +3,17 @@
 namespace Teamleader\Entities\Invoicing;
 
 use Teamleader\Actions\FindAll;
+use Teamleader\Actions\FindById;
+use Teamleader\Actions\Storable;
 use Teamleader\Model;
 
 class CreditNote extends Model
 {
     use FindAll;
+    use FindById;
+    use Storable;
+
+    const TYPE = 'credit_note';
 
     protected $fillable = [
         'id',
@@ -19,6 +25,7 @@ class CreditNote extends Model
         'paid',
         'paid_at',
         'invoicee',
+        'discounts',
         'total',
         'grouped_lines',
         'currency_exchange_rate',
@@ -30,4 +37,27 @@ class CreditNote extends Model
      * @var string
      */
     protected $endpoint = 'creditNotes';
+
+    public function download($format = "pdf")
+    {
+        $arguments = [
+            'id'     => $this->attributes['id'],
+            'format' => $format,
+        ];
+
+        $result = $this->connection()->post($this->getEndpoint() . '.download', json_encode($arguments, JSON_FORCE_OBJECT));
+
+        return $result;
+    }
+
+    public function file($format = "pdf")
+    {
+        $result = $this->download($format);
+
+        if (isset($result['data']) && isset($result['data']['location'])) {
+            return file_get_contents($result['data']['location']);
+        }
+
+        return false;
+    }
 }
